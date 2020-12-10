9cefbc769900d8375d26fa7e7e1c39990fb794fa
cynthiamjk <56000459+cynthiamjk@users.noreply.github.com>
2020-12-10 15:33:31 +0100

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.*;
+import java.math.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+
+ 
+
+ 
+class Solution {
+
+  
+
+    public static void main(String[] args) {
+      
+     
+        System.out.println("hi");
+    }
+    
+}
